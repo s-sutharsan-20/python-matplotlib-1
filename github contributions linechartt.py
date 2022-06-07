0bac0b33830f38144a40bc237a2aa3ae55978bca
Sutharsan.S <78362509+s-sutharsan-20@users.noreply.github.com>
2022-06-07 15:25:20 +0530

    Add files via upload

---
+++ github contributions linechartt.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+contributions=[4,4,8,6,5,5,6]
+date=['29/05/2022','30/05/2022','31/05/2022','01/06/2022','02/06/2022','03/06/2022','04/06/2022']
+plt.plot(date,contributions)
+plt.title("GITHUB Contributions")
+plt.ylabel("Contributions")
+plt.xlabel("Date")
+plt.show()
